00fe7435add6ec8f5dfdc86dddd3727fd2804e8e
blancocd <blancocedi@gmail.com>
2025-07-04 16:13:25 +0200

    test with minimal prompt input but latents pipeline

--- vae_inpainting_tests/test_inpaint_kontest.py
+++ vae_inpainting_tests/test_inpaint_kontest.py
@@ -2,7 +2,7 @@ import torch
 from diffusers import FluxKontextInpaintPipeline
 from diffusers.utils import load_image
 from PIL import Image
-from 
+from reconstruction_pipelines.flux_inpaint_kontext import FluxKontextInpaintPipeline
 
 pipe = FluxKontextInpaintPipeline.from_pretrained("black-forest-labs/FLUX.1-Kontext-dev", torch_dtype=torch.bfloat16)
 pipe.to("cuda")
@@ -11,5 +11,7 @@ prompt = ""
 input_image = load_image('/mnt/lustre/work/ponsmoll/pba534/ffgarments_datagen/data/4D-DRESS/00122_Outer/images/train_0000.png')
 mask_image = Image.new("RGB", (input_image.width, input_image.height))
 
-image = pipe(prompt=prompt, image=input_image, mask_image=mask_image).images[0]
-image.save("kontext_inpainting_ref.png")
+output = pipe(prompt=prompt, image=input_image, mask_image=mask_image)
+output["denoised_image"].save("fill_reconstruction_denoised_{seed}.png")
+output["reconstructed_image"].save("fill_reconstruction_vae_only_{seed}.png")
+print("Saved 'fill_reconstruction_denoised.png' and 'fill_reconstruction_vae_only.png' with seed {seed}")
